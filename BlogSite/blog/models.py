1637baf210d214b323ec7c77236ea267627ee3b3
bing yang <bingyang9258@gmail.com>
2021-09-16 18:21:02 -0400

    add post models

--- BlogSite/blog/models.py
+++ BlogSite/blog/models.py
@@ -1,17 +1,27 @@
 import uuid
+from django.conf import settings
 from django.db import models
+from django.utils.timezone import now
 
 
-class User(models.Model):
-    uuid = models.UUIDField(unique=True, default=uuid.uuid4)
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=16)
-    email = models.EmailField()
-    created_at = models.DateTimeField("datetime created")
+# class CustomUser(models.Model):
+#     uuid = models.UUIDField(unique=True, default=uuid.uuid4)
+#     username = models.CharField(max_length=20)
+#     password = models.CharField(max_length=16)
+#     email = models.EmailField()
+#     created_at = models.DateTimeField("datetime created")
 
 
 class Post(models.Model):
-    uuid = models.UUIDField(unique=True, default=uuid.uuid4)
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
 
     title = models.CharField(max_length=100)
-    published_at = models.DateTimeField("date published")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
+    content_body = models.TextField(default="")
+    published_at = models.DateTimeField(default=now)
+    edited_at = models.DateTimeField(default=now, editable=True)
+    likes_num = models.IntegerField(default=0)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
