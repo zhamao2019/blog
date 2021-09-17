1e0c07138aa2540886e4025d19bc2eb62451db02
bing yang <bingyang9258@gmail.com>
2021-09-17 14:05:50 -0400

    add User model, fixed models structure

--- BlogSite/blog/models.py
+++ BlogSite/blog/models.py
@@ -4,14 +4,20 @@ from django.db import models
 from django.utils.timezone import now
 
 
+class PlatformUser(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    avatar = models.ImageField(null=True, blank=True)
+
+
 class Post(models.Model):
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
 
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, null=False, blank=False)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
-    content_body = models.TextField(default="")
+    content_body = models.TextField(null=False, blank=False)
     published_at = models.DateTimeField(default=now)
-    edited_at = models.DateTimeField(default=now, editable=True)
+    edited_at = models.DateTimeField(default=now)
     likes_num = models.IntegerField(default=0)
     is_deleted = models.BooleanField(default=False)
 
@@ -22,6 +28,6 @@ class Post(models.Model):
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    comment = models.TextField(default="", max_length=500)
+    comment = models.TextField(max_length=500, null=False, blank=False)
     created_at = models.DateTimeField(default=now)
 
